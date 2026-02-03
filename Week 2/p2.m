2a31c0daf01d4d6b7c27399ee17fc6f75965bbc2
deon-does-dev <182770454+deon-does-dev@users.noreply.github.com>
2026-02-03 16:21:40 +0000

    Created and added basic calculation of audio data + Used built-in audioread() function to create an array of sample values and each sample's frequency + Calculated size information by analysing sample and channel counts + Calculated duration of sample by dividing no. of samples by sampling frequency + Calculated length of sample by multiplying no. of samples by duration of 1 sample

---
+++ Week 2/p2.m
@@ -0,0 +1,15 @@
+% Look at import_wav.m (in Class Materials), and use this as a starting
+% point to load your own audio files. 
+
+% import audio data as a 2D array
+[sig, fs] = audioread("handel.wav");
+
+samples = size(sig, 1);      % count no. of rows 
+channels = size(sig, 2);     % count no. of columns
+
+% no. of samples / sampling frequency = duration
+duration = samples/fs;
+
+% calculate length of sample
+t = (0:(samples - 1)) * (1/fs); % time = no. of samples * duration of 1 sample
+
